15618b831192ea42aa10bbddec115997141e18df
coder-sanju <sanju2help@gmail.com>
2020-08-26 02:12:28 +0530

    update

--- codesession_main/codesession/urls.py
+++ codesession_main/codesession/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path('Programs',views.Programs,name="Programs"),
     path('contact',views.contact,name="contact"),
     path('services',views.service,name="services"),
-    path('1_semester',views.sem1_1year,name="1_semester")
+    path('1_semester',views.sem1_1year,name="1_semester"),
+    path('2_semester',views.sem2_1year,name="2_semester"),
+    path('civil_2nd',views.civil_2nd ,name="civil_2nd"),
+    path('cse_2nd',views.cse_2nd ,name="cse_2nd"),
+    path('me_2nd',views.me_2nd ,name="me_2nd"),
+    path('el_2nd',views.el_2nd ,name="el_2nd"),
+    path('ee_2nd',views.ee_2nd ,name="ee_2nd"),
+    path('it_2nd',views.it_2nd ,name="it_2nd"),
 ]
--- codesession_main/codesession/views.py
+++ codesession_main/codesession/views.py
@@ -20,3 +20,24 @@ def service(request):
 
 def sem1_1year(request):
     return render(request,"html_files/sem1_1year.htm")
+
+def sem2_1year(request):
+    return render(request,"html_files/2nd_sem.htm")
+
+def civil_2nd(request):
+    return render(request,"html_files/civil_2nd.htm")
+
+def cse_2nd(request):
+    return render(request,"html_files/cse_2nd.htm")
+
+def ee_2nd(request):
+    return render(request,"html_files/ee_2nd.htm")
+
+def el_2nd(request):
+    return render(request,"html_files/el_2nd.htm")
+
+def me_2nd(request):
+    return render(request,"html_files/me_2nd.htm")
+
+def it_2nd(request):
+    return render(request,"html_files/it_2nd.htm")

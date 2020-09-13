eea4a9dd91f5d8a9f85aa6365e53f609b567f3d8
coder-sanju <sanju2help@gmail.com>
2020-09-14 01:05:35 +0530

    update

--- codesession_main/codesession_main/settings.py
+++ codesession_main/codesession_main/settings.py
@@ -128,7 +128,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/') 
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static'),
 ]
